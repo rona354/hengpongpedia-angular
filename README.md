0268a34d3eb3af628bb4cc468dd87f59748e39c9
rona354 <royanfauzan@gmail.com>
2019-09-23 08:40:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hengpongpedia
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hengpongpedia)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Ipin X',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Samsul S7',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Nokai',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
